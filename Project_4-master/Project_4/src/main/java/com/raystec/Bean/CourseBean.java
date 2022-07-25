58816e83812c346134ac31150dd0bfd89a172b18
Avi077777 <saxenaavinash010@gmail.com>
2022-07-25 16:15:51 +0530

    proj4

---
+++ Project_4-master/Project_4/src/main/java/com/raystec/Bean/CourseBean.java
@@ -0,0 +1,47 @@
+package com.raystec.Bean;
+
+public class CourseBean extends BaseBean {
+
+	private String Course_Name;
+	private String Discription;
+	private String Duration;
+	
+	public CourseBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCourse_Name() {
+		return Course_Name;
+	}
+
+	public void setCourse_Name(String course_Name) {
+		Course_Name = course_Name;
+	}
+
+	public String getDiscription() {
+		return Discription;
+	}
+
+	public void setDiscription(String discription) {
+		Discription = discription;
+	}
+
+	public String getDuration() {
+		return Duration;
+	}
+
+	public void setDuration(String duration) {
+		Duration = duration;
+	}
+
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return id + "";
+	}
+
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return Course_Name;
+	}
+
+}
